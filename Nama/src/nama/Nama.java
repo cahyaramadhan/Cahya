426687950a3bfd7e9de5bca7a6f9bdc180efe3c0
cahyaramadhan <36650685+cahyaramadhan@users.noreply.github.com>
2018-02-20 10:08:46 +0700

    Penampil waktu
    Latihan pak zul

--- Nama/src/nama/Nama.java
+++ Nama/src/nama/Nama.java
@@ -15,7 +15,15 @@ public class Nama {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        long totalMiliseconds = System.currentTimeMillis(); //waktu diambil dari mili seconds
+        long totalSeconds = totalMiliseconds / 1000;
+        long currentSecond = (int)(totalSeconds % 60); //int buat apa?????????
+        long totalMinutes = totalSeconds / 60;
+        long currentMinute = totalMinutes % 60;
+        long totalHours = totalMinutes / 60;
+        long currentHour = (totalHours % 24) + 7; // + 7 Karena Indonesia
+        System.out.println("Current time is " + currentHour + ":" + currentMinute + ":" + currentSecond + " GMT");
     }
     
 }
