df48b0434103b0555abfd97a7c3e3c639a376b49
cahyaramadhan <36650685+cahyaramadhan@users.noreply.github.com>
2018-02-27 13:33:50 +0700

    Penambahan Link Data2 Food Court

---
+++ Chapter3/src/SampleIfDemo.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author lenovo
+ */
+public class SampleIfDemo {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an integer: ");
+        int number = input.nextInt();
+        if (number % 5 == 0) {
+            System.out.println("HiFive");
+        }
+        if (number % 2 == 0) {
+            System.out.println("HiEven");
+        }
+    }
+}
